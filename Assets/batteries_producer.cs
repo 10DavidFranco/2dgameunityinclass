3aa03fbcaed70d5e64bf0fa32f6d003a70cf9954
10DavidFranco <david.franco02@utrgv.edu>
2025-02-27 14:32:58 -0600

    first commit

---
+++ Assets/batteries_producer.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class batteries_producer : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public float r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            spawObject();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(Input.GetKeyDown(KeyCode.B))
+        // {
+        //   spawObject();
+        // }
+    }
+
+    void spawObject()
+    {
+
+        Vector3 randomPos = Random.insideUnitCircle * r;
+        Instantiate(itemPrefab, randomPos, Quaternion.identity);
+
+    }
+}
---
+++ Assets/scripts/Hello_world.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello_world : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    void Update()
+    {
+        Debug.Log("updating");
+    }
+}
